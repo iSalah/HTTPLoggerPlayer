b9435fa3e86fe8e46b01ceda85aebc3355a057f0
Salah Kaci <salah91@gmail.com>
2017-06-26 00:01:20 +0200

    Initial commit

---
+++ HTTPLoggerPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HTTPLoggerPlayer
+//
+//  Created by Salah on 2017-06-26.
+//  Copyright © 2017 DevSK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
