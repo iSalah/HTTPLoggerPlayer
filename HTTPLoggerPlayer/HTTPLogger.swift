86fc5c764e0f6c64f263342cff4d2eaae2e121fd
Salah Kaci <salah91@gmail.com>
2017-06-26 00:17:15 +0200

    Register custom URLProtocol

---
+++ HTTPLoggerPlayer/HTTPLogger.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPLogger.swift
+//  HTTPLoggerPlayer
+//
+//  Created by Salah on 2017-06-26.
+//  Copyright © 2017 DevSK. All rights reserved.
+//
+
+import Foundation
+
+class HTTPLogger: URLProtocol {
+
+}
