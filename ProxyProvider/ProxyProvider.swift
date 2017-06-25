c777c2e6cb03d06d7e1d44e02fd759e7d86e58e5
Salah Kaci <salah91@gmail.com>
2017-06-26 00:50:39 +0200

    Add NEAppProxyProvider extension

---
+++ ProxyProvider/ProxyProvider.swift
@@ -0,0 +1,15 @@
+//
+//  ProxyProvider.swift
+//  HTTPLoggerPlayer
+//
+//  Created by Salah on 2017-06-26.
+//  Copyright © 2017 DevSK. All rights reserved.
+//
+
+import NetworkExtension
+
+class NetworkProxyProvider: NEAppProxyProvider {
+    
+    
+    
+}
