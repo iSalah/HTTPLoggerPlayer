5142af204d63c5e44449f3045b5b770eedf15b4e
Salah Kaci <salah91@gmail.com>
2017-06-26 00:38:42 +0200

    Add NEFilterDataProvider extension

---
+++ FilterData/FilterData.swift
@@ -0,0 +1,13 @@
+//
+//  FilterData.swift
+//  HTTPLoggerPlayer
+//
+//  Created by Salah on 2017-06-26.
+//  Copyright © 2017 DevSK. All rights reserved.
+//
+
+import NetworkExtension
+
+class FilterData: NEFilterDataProvider {
+    
+}
