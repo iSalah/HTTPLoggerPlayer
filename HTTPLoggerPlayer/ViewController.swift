f63f75806961272e274c151c2fc283f9a9574ef5
Salah Kaci <salah91@gmail.com>
2017-06-26 00:08:33 +0200

    Add player & play stream

--- HTTPLoggerPlayer/ViewController.swift
+++ HTTPLoggerPlayer/ViewController.swift
@@ -7,10 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVPlayer?
+    let streamURL = "https://wowza-cloudfront.streamroot.io/liveorigin/stream4/playlist.m3u8"
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupPlayer()
+    }
     
+    private func setupPlayer() {
+        guard let url = URL(string: self.streamURL) else { return }
+        
+        self.player = AVPlayer(url: url)
+        let playerLayer = AVPlayerLayer(player: self.player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        self.player!.play()
+    }
 }
 
